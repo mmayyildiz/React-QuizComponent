b3f97bc9dec5b6e8a86be15eb490afe30a39be66
meltem ayyıldız <mmeltemayyildiz@gmail.com>
2019-09-11 01:10:51 +0300

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,8 @@ class Quiz extends Component{
     }
 
     render() {
-        const isQuizEnd = true;
+        const isQuizEnd = ((this.state.quiz_position - 1) === 
+          quizData.quiz_questions.length);
         return (
             <div>
                 {isQuizEnd ? <isQuizEnd /> : 
